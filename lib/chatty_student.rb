14a73098d416f0665a98db9e55bdaddced14a8de
Roberto Garza <robertg4567@hotmail.com>
2018-08-07 18:12:40 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,8 +4,14 @@ class ChattyStudent < Student
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   def raise_hand
-    loop { super }.times(10)
+    i = 1
+    loop do
+      super
+      i+=1
+      if i == 10
+        break 
+      end 
     end
-    
+
   end
 end
